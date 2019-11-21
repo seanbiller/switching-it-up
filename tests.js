c581729cfbf33c654430def74168f84a8be1ffd5
Sean Biller <seanmbiller@gmail.com>
2019-11-20 21:34:41 -0500

    finished in class exercise

--- tests.js
+++ tests.js
@@ -1,7 +1,30 @@
 var mocha = require('mocha')
 var calculateScore = require('./scoring').calculateScore
 var expect = require('chai').expect
-var { describe, it } = mocha
+var {
+  describe,
+  it
+} = mocha
+var calculateRushingScore = require('./scoring').calculateRushingScore
+
+describe('Scoring', function () {
+  it('returns total rushing score', function () {
+    var player = {
+      position: 'RB',
+      stats: {
+        rushing: {
+          attempts: 3,
+          yards: 22,
+          touchdowns: 1,
+          fumbles: 0
+        }
+      }
+    }
+    var points = calculateRushingScore(player)
+    expect(points).to.equal(8.2)
+  })
+})
+
 
 describe('Scoring', function () {
   it('returns the score for a quarterback', function () {
@@ -17,7 +40,12 @@ describe('Scoring', function () {
           touchdowns: 3,
           interceptions: 0
         },
-        rushing: { attempts: 3, yards: 22, touchdowns: 1, fumbles: 0 }
+        rushing: {
+          attempts: 3,
+          yards: 22,
+          touchdowns: 1,
+          fumbles: 0
+        }
       }
     }
 
@@ -32,11 +60,31 @@ describe('Scoring', function () {
       position: 'RB',
       team: 'Buffalo',
       stats: {
-        rushing: { attempts: 18, yards: 103, touchdowns: 2, fumbles: 0 },
-        receiving: { receptions: 6, yards: 37, touchdowns: 0, fumbles: 0 },
+        rushing: {
+          attempts: 18,
+          yards: 103,
+          touchdowns: 2,
+          fumbles: 0
+        },
+        receiving: {
+          receptions: 6,
+          yards: 37,
+          touchdowns: 0,
+          fumbles: 0
+        },
         return: {
-          kickreturn: { returns: 0, yards: 0, touchdowns: 0, fumbles: 0 },
-          puntreturn: { returns: 0, yards: 0, touchdowns: 0, fumbles: 0 },
+          kickreturn: {
+            returns: 0,
+            yards: 0,
+            touchdowns: 0,
+            fumbles: 0
+          },
+          puntreturn: {
+            returns: 0,
+            yards: 0,
+            touchdowns: 0,
+            fumbles: 0
+          },
         },
       }
     }
@@ -52,11 +100,31 @@ describe('Scoring', function () {
       position: 'WR',
       team: 'Seattle',
       stats: {
-        rushing: { attempts: 0, yards: 0, touchdowns: 0, fumbles: 0 },
-        receiving: { receptions: 6, yards: 91, touchdowns: 1, fumbles: 0 },
+        rushing: {
+          attempts: 0,
+          yards: 0,
+          touchdowns: 0,
+          fumbles: 0
+        },
+        receiving: {
+          receptions: 6,
+          yards: 91,
+          touchdowns: 1,
+          fumbles: 0
+        },
         return: {
-          kickreturn: { returns: 2, yards: 16, touchdowns: 0, fumbles: 1 },
-          puntreturn: { returns: 3, yards: 107, touchdowns: 1, fumbles: 0 },
+          kickreturn: {
+            returns: 2,
+            yards: 16,
+            touchdowns: 0,
+            fumbles: 1
+          },
+          puntreturn: {
+            returns: 3,
+            yards: 107,
+            touchdowns: 1,
+            fumbles: 0
+          },
         },
       }
     }
@@ -72,7 +140,12 @@ describe('Scoring', function () {
       position: 'TE',
       team: 'New England',
       stats: {
-        receiving: { receptions: 8, yards: 137, touchdowns: 2, fumbles: 0 },
+        receiving: {
+          receptions: 8,
+          yards: 137,
+          touchdowns: 2,
+          fumbles: 0
+        },
       }
     }
 
@@ -87,8 +160,14 @@ describe('Scoring', function () {
       position: 'K',
       team: 'San Francisco',
       stats: {
-        fieldgoals: { attempts: 3, made: 2 },
-        xp: { attempts: 2, made: 2 }
+        fieldgoals: {
+          attempts: 3,
+          made: 2
+        },
+        xp: {
+          attempts: 2,
+          made: 2
+        }
       }
     }
 
